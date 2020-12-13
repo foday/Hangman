3b5662e801825e23e36a6dc22f9241d3a4cf1e50
foday <foday_s@yahoo.com>
2020-12-13 11:03:04 -0600

    While loop
    Now that we can get the user input we want the user to be able to keep guessing until they get the word in order to make that happen we are going to use a while loop so the user can keep on guessing until he guess the right word. we are removing a word letter from set words which we haven't seen yet. The condition that needs to be satisfied after the user gets all the word is the length or 'len' of word letter is = 0. so this while loop will continue to loop and substract used letters in alphabet.

--- hangman.py
+++ hangman.py
@@ -16,6 +16,15 @@ def hangman():
     used_letters = set() #what the user has guessed
     
     #getting user input
+    while len(word_letters) > 0:
+        #letters used
+        #' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print('You have used these letters: ', ' '.join(used_letters))
+
+        #what current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ''.join(word_list))
+    
     user_letter = input('Guess a letter: ').upper()
     if user_letter in alphabet - used_letters:
         used_letters.add(user_letter)
@@ -27,6 +36,8 @@ def hangman():
 
     else:
         print('invalid character. please try again ')
+
+
 
 user_input = input('Type something: ')
 print(user_input)
