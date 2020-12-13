caab65af3410e3d80b0913bfc7cf32cc4962980c
foday <foday_s@yahoo.com>
2020-12-13 09:10:19 -0600

    The while loop
    Description: The while loop continues to loop between all the words until there is no more space or -. That is how the compupter will choose a word out of the 1000s of words in the file. As long as there is - or ' ' in 'word' keep choosing this word once it stops iterating we will get a word that no longer has a - or space in it. Once it finds the word it returns it

--- hangman.py
+++ hangman.py
@@ -3,3 +3,5 @@ from words import words
 
 def get_valid_word(words):
     word = random.choice(words) #randomly chooses something from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
