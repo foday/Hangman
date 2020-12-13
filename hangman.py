438ec1b9537bf1ffa39457efe17620257d2278a3
foday <foday_s@yahoo.com>
2020-12-13 10:47:41 -0600

    Getting user input
    Description: What we can do is ask for user input User the user_input as input. if user_letter in the alphabet is a valid character that i havent used yet i am going to add to user set. else if user_letter in usedPP_letters jsut let them know that you already used this character. else if they type something that is not in the alphabet and is not in the used letter that they guessed that means they typed in the wrong character so we just let them know they typed in an invalid character

--- hangman.py
+++ hangman.py
@@ -14,3 +14,14 @@ def hangman():
     word_letters = set(word) #letters in the word
     alphabet = set(string.ascii_uppercase)
     used_letters = set() #what the user has guessed
+    
+    #getting user input
+    user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters
+user_input = input('Type something: ')
+print(user_input)
