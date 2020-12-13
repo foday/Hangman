1f47c3bf246fa0d5f1aa74cd59fa11cdea02e505
foday <foday_s@yahoo.com>
2020-12-13 09:13:45 -0600

    return word.upper()
    Description: All the letters are going to be uppercase so when the word is returned all of the letters will be uppercase thanks for the upper() function

--- hangman.py
+++ hangman.py
@@ -5,3 +5,5 @@ def get_valid_word(words):
     word = random.choice(words) #randomly chooses something from the list
     while '-' in word or ' ' in word:
         word = random.choice(words)
+
+    return word.upper()
