2be12ca364fcf47d4f753ab0a608f35eb27d16c6
foday <foday_s@yahoo.com>
2020-12-15 13:54:28 -0600

    Intro
    Description: Some of the words from words. py have ' ' or spaces and - or dashes in between the words, so a function needs to be created that has a while loop that continues to iterate while there is - or space in between the words. This function takes care of the words with spaces and dashes in between them, so it will continue to pick words or iterate from the list until there isn't any with - or space in between those words.

--- hangman.py
+++ hangman.py
@@ -2,3 +2,9 @@ import random
 from words import words
 import string
 
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word
