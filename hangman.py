3f525d6d40cf631ac81f3dc98958a43ea4a8c579
foday <foday_s@yahoo.com>
2020-12-15 15:00:48 -0600

    Controlling Length of word_letters with a while loop
    Description: Now we can get input from the user we want the user to keep on guessing until the user can get the word so a loop will be used. While loop will iterate around the code.
    Cause everytime we are removing a letter from word_letters the while loop will continue to iterate until it is no longer greater than 0.
    .join(used_letters)) method the letters that have been used to show a horizontal list.
    word_list is a list where every single letter is shown and every letter they have not guessed show dashes.

--- hangman.py
+++ hangman.py
@@ -16,14 +16,27 @@ def hangman():
     used_letters = set() #keep track of what the user has guessed
 
     #getting user input
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
+    while len(word_letters) > 0:
+        #letters used
+        #' '.join(['a', 'b', 'cd']) --> 'a b cd'
+        print('You have used these letters: ', ' '.join(used_letters))
 
-    elif user_letter in used_letters:
-        print('You have already used that character. Try again. ')
+        #what current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
 
-    else:
-        print('Invalid character. Try again.')
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in used_letters:
+            print('You have already used that character. Try again. ')
+
+        else:
+            print('Invalid character. Try again.')
+
+#user_input = input('Type something ')
+#print(user_input)
+hangman()
